a6cb5dc77bf3bc0007b627db313905818b59e5f7
SimonianDA <71718753+SimonianDA@users.noreply.github.com>
2025-06-05 13:49:21 +0300

    Add files via upload

---
+++ src/PKIS1/Models/WordSearchResult.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programming_of_corporate_industrial_systems_Practice_1.Models
+{
+    /// <summary>
+    /// Результат анализа текстового файла, содержащий общее количество слов и количество вхождений заданного слова.
+    /// </summary>
+    public class WordSearchResult
+    {
+        /// <summary>
+        /// Общее количество слов в тексте.
+        /// </summary>
+        public int TotalWordCount { get; set; }
+
+        /// <summary>
+        /// Количество повторений искомого слова в тексте.
+        /// </summary>
+        public int TargetWordCount { get; set; }
+    }
+}
